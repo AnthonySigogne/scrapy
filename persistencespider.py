320ced6f44eeec1be3a8439d5e962ea369c9d9f5
anthony sigogne <anthonysigogne@MBP-de-anthony.lan>
2017-07-22 10:26:04 +1100

    add persistence crawlers

---
+++ persistencespider.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import scrapy
+import elasticsearch
+
+from elasticsearch import Elasticsearch
+es = Elasticsearch(hosts=["localhost"], http_auth=("elastic", "changeme"), port=9200)
+
+class PersistenceSpider(scrapy.Spider):
+    name = 'persistencespider'
+    start_urls = []
+
+    def __init__(self, *args, **kwargs):
+        super(PersistenceSpider, self).__init__(*args, **kwargs)
+        # read input file that contains pages to crawl
+        with open(kwargs.get('file')) as f :
+            self.start_urls = f.read().splitlines()
+
+    def parse(self, response):
+        doc = {
+            "url": response.url,
+            "title": response.css('title::text').extract_first(),
+            "description": response.css("meta[name=description]::attr(content)").extract_first()
+        }
+        res = es.index(index="web", doc_type='page', body=doc)
+        print(res['created'])
+        yield doc
